5369af39de21af2ac4fc51092a262bcff2809b79
Geoffrey V. Cestaro <gcint2@outlook.com>
2017-09-20 12:01:31 -0400

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
+
+  post 'create_users' => 'welcome#create_users'
+  get 'users' => 'welcome#users'
+
   get 'welcome/index'
 
-  get 'welcome/users'
-
-  get 'welcome/products'
+  post 'products' => 'welcome#create_products'
+  get 'products' => 'welcome#products'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
